f82fc1eefe806f1dd9ae0724fcf237e5fcbda42b
shingo <40855834+KatanoShingo@users.noreply.github.com>
2024-05-28 06:33:25 +0900

    海に落ちたら初めから再開するように修正

---
+++ Assets/Script/FallDetection.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallDetection : MonoBehaviour
+{
+    public float fallThreshold = -50.0f; // 奈落の高さのしきい値
+
+    void Update()
+    {
+        // プレイヤーの高さがしきい値よりも低いかどうかをチェック
+        if (transform.position.y < fallThreshold)
+        {
+            // シーンを再リロード
+            ReloadScene();
+        }
+    }
+
+    void ReloadScene()
+    {
+        // 現在のシーンを取得
+        Scene currentScene = SceneManager.GetActiveScene();
+        // 現在のシーンを再リロード
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
