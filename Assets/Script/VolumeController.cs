5b108482531810971b7bb9af43aafb60d2bff0d6
shingo <40855834+KatanoShingo@users.noreply.github.com>
2024-05-30 10:04:20 +0900

    SEも音量調整できるように対応

--- Assets/Script/VolumeController.cs
+++ Assets/Script/VolumeController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class VolumeController : MonoBehaviour
 {
     public AudioSource audioSource; // AudioSourceを格納するための変数
+    public AudioSource audioSourceSe; // AudioSourceを格納するための変数
     public float volumeChangeSpeed = 0.1f; // 音量変更の速さ
 
     void Update()
@@ -12,11 +13,13 @@ public class VolumeController : MonoBehaviour
         {
             // Kキーが押されたとき、音量を増加
             audioSource.volume = Mathf.Clamp(audioSource.volume + volumeChangeSpeed * Time.deltaTime, 0f, 1f);
+            audioSourceSe.volume = Mathf.Clamp(audioSource.volume + volumeChangeSpeed * Time.deltaTime, 0f, 1f);
         }
         else if (Input.GetKey(KeyCode.J))
         {
             // Jキーが押されたとき、音量を減少
             audioSource.volume = Mathf.Clamp(audioSource.volume - volumeChangeSpeed * Time.deltaTime, 0f, 1f);
+            audioSourceSe.volume = Mathf.Clamp(audioSource.volume - volumeChangeSpeed * Time.deltaTime, 0f, 1f);
         }
     }
 }
